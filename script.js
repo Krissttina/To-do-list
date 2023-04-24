22b4e6cc564f7563df4082dc188bde1326224f0d
Kristina <krismetodieva123@gmail.com>
2023-04-24 18:56:19 +0300

    quick update
    keeping the updated code before adding new functionality

--- script.js
+++ script.js
@@ -1,53 +1,45 @@
-var myNodelist = document.getElementsByTagName("LI");
-var i;
-for (i = 0; i < myNodelist.length; i++) {
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  myNodelist[i].appendChild(span);
-}
-
 // Click on a close button to hide the current list item
-var close = document.getElementsByClassName("close");
-var i;
+let close = document.getElementsByClassName("close");
+
 for (i = 0; i < close.length; i++) {
   close[i].onclick = function() {
-    var div = this.parentElement;
+    let div = this.parentElement;
     div.style.display = "none";
   }
 }
 
-// Add a "checked" symbol when clicking on a list item
-var list = document.querySelector('ul');
-list.addEventListener('click', function(ev) {
-  if (ev.target.tagName === 'LI') {
-    ev.target.classList.toggle('checked');
+// Add a "checked" symbol when clicking on a list item - not working
+const list = document.querySelector('ul');
+list.addEventListener('click', function(e) {
+  if (e.target.tagName === 'LI') {
+    e.target.classList.toggle('checked');
   }
-}, false);
+}, false); //add radio button for checking
 
-// Create a new list item when clicking on the "Add" button
+// Create new list item when Add button is clicked
 function newElement() {
-  var li = document.createElement("li");
-  var inputValue = document.getElementById("myInput").value;
-  var t = document.createTextNode(inputValue);
-  li.appendChild(t);
-  if (inputValue === '') {
+  const li = document.createElement("li");
+  const inputValue = document.getElementById("myInput").value;
+  const text = document.createTextNode(inputValue);
+  li.appendChild(text);
+
+  if (inputValue == '') {
     alert("You must write something!");
   } else {
-    document.getElementById("myUL").appendChild(li);
+    document.getElementById("list").appendChild(li);
   }
-  document.getElementById("myInput").value = "";
 
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
+  document.getElementById("myInput").value = ""; //empty imput fields
+
+  const span = document.createElement("span");
+  const cross = document.createTextNode("\u00D7"); //cross
   span.className = "close";
-  span.appendChild(txt);
+  span.appendChild(cross);
   li.appendChild(span);
 
   for (i = 0; i < close.length; i++) {
     close[i].onclick = function() {
-      var div = this.parentElement;
+      let div = this.parentElement;
       div.style.display = "none";
     }
   }
